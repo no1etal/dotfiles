415093c0e0ff34afa65cb79cd6672e72d05a9f2d
no1etal <riker.tom@proton.me>
2023-11-08 20:27:31 +0100

    ftmuxp

--- zsh/scripts.sh
+++ zsh/scripts.sh
@@ -2,3 +2,23 @@
 compress() {
 tar cvzf $1.tar.gz $1
 }
+
+ftmuxp() {
+if [[ -n $TMUX ]]; then
+return
+fi
+# get the IDs
+ID="$(ls $XDG_CONFIG_HOME/tmuxp | sed -e 's/\.yml$//')"
+if [[ -z "$ID" ]]; then
+tmux new-session
+fi
+create_new_session="Create New Session"
+ID="${create_new_session}\n$ID"
+ID="$(echo $ID | fzf | cut -d: -f1)"
+if [[ "$ID" = "${create_new_session}" ]]; then
+tmux new-session
+elif [[ -n "$ID" ]]; then
+# Rename the current urxvt tab to session name
+tmuxp load "$ID"
+fi
+}
